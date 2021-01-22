0b7eaf2dcb463c445dff9f7ceec611065d15833c
Anton.Fursov <simpletonn@live.ru>
2021-01-22 09:52:33 +0300

    3 готово

--- git.repo/4_3.py
+++ git.repo/4_3.py
@@ -1,37 +1,44 @@
 
 queries = [
+    'ffff',
     'смотреть сериалы онлайн',
     'новости спорта',
     'афиша кино',
     'курс доллара',
+    'курс доллара3',
+    'курс доллара2',
+    'курс доллара1',
     'сериалы этим летом',
     'курс по питону',
     'сериалы про спорт',
-    'ffff'
 ]
 #считаем  количество  слов в кажом запросе и создаем словарь где  запрос является ключем и  количество слов в нем значением
 queries_dict = {}
 for temp in queries:
     queries_dict[temp] = len(temp.split())
-print(queries_dict)
+#print(queries_dict)
 
 # считаем  общее количество запроосов и создем ещё один словарь с ключем количество слов и  значением количество запросов
 proportion100=0
-temp2 = {}
+count_queries_dict = {}
 for queries_dict_key, queries_dict_value in queries_dict.items():
-    proportion100 = proportion100 + queries_dict_value
-    if queries_dict_key in temp2:
-        temp2[queries_dict_value] = temp2[queries_dict_value]+1
+    proportion100 = proportion100 + 1
+    if queries_dict_value in count_queries_dict:
+        count_queries_dict[queries_dict_value] = count_queries_dict[queries_dict_value]+1
     else:
-        temp2[queries_dict_value] = 1
+        count_queries_dict[queries_dict_value] = 1
+#print(count_queries_dict)
+#print(proportion100)
 
-print(temp2)
-print_list = []
-sum_proporcia100=0
-for count_queries,s_proporcia in temp2.items():
-    proportion100 = proportion100+ s_proporcia
-    print_list.append(f"")
+proportion = 0
+for count_queries_key, count_queries_value in count_queries_dict.items():
+    proportion = count_queries_value*100/proportion100
+    print(f"Колличество слов в запросе {count_queries_key} процентная составляющая {round(proportion,3)} %")
 
-print(sum_proporcia100)
-print((proportion100))
+
+
+
+
+
+
 
