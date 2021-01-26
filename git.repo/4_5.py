6db913ead4e2cdf7fdd185b4eea5ec776b4d19cb
Anton.Fursov <simpletonn@live.ru>
2021-01-26 13:32:58 +0300

    4 готово

--- git.repo/4_5.py
+++ git.repo/4_5.py
@@ -1,11 +1,12 @@
 from pprint import pprint
 #source = ['2018-01-01', 'yandex', 'cpc', 100]
-source = ['2018-01-01', 'yandex']
+source = ['2018-01-01']
 if len(source)==1:
-    pritn("Словарь невозможен")
-temp_dict = source[-1]
-del(source[-1])
-while len(source) >= 1:
+    print ("Словарь невозможен")
+else:
+    temp_dict = source[-1]
+    del(source[-1])
+    while len(source) >= 1:
         temp_dict = {source[-1]: temp_dict}
         del source[-1]
 
