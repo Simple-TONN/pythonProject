fe18239575390fa56ebefbb860b4e6b5e10d40a9
Anton.Fursov <simpletonn@live.ru>
2021-03-11 11:09:11 +0300

    4_4

--- git.repo/4_4.py
+++ git.repo/4_4.py
@@ -4,5 +4,5 @@ max_stats_value = []
 for stats_values in stats.values():
     max_stats_value.append(stats_values)
 for resultkey, resultvalue in stats.items():
-    if resultvalue== max(max_stats_value):
+    if resultvalue == max(max_stats_value):
         print(resultkey)
