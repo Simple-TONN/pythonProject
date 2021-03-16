35047dab5853b080dc2a518b92a82a7282e4121d
Anton.Fursov <simpletonn@live.ru>
2021-03-16 11:29:39 +0300

    Work

---
+++ git.repo/8_API/8_1.py
@@ -0,0 +1,21 @@
+import requests
+import pprint
+import json
+
+token = "2619421814940190"
+
+her = ["Hulk", "Captain America", "Thanos"]
+res_dict ={}
+for each_her in her:
+    response = requests.get(f'https://superheroapi.com/api/{token}/search/{each_her}')
+    res_dict[each_her]=int(response.json()['results'][0]['powerstats']['intelligence'])
+
+print(res_dict)
+print(max(res_dict.values()))
+print(max(res_dict.keys()))
+
+
+
+
+
+
