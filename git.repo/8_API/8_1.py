f38a929e62947534d572024fdaf63eb85f1456fd
Anton.Fursov <simpletonn@live.ru>
2021-03-16 11:31:44 +0300

    Work

--- git.repo/8_API/8_1.py
+++ git.repo/8_API/8_1.py
@@ -3,17 +3,11 @@ import requests
 token = "2619421814940190"
 
 her = ["Hulk", "Captain America", "Thanos"]
-res_dict ={}
+res_dict = {}
 for each_her in her:
     response = requests.get(f'https://superheroapi.com/api/{token}/search/{each_her}')
-    res_dict[each_her]=int(response.json()['results'][0]['powerstats']['intelligence'])
+    res_dict[each_her] = int(response.json()['results'][0]['powerstats']['intelligence'])
 
 print(res_dict)
 print(max(res_dict.values()))
 print(max(res_dict.keys()))
-
-
-
-
-
-
